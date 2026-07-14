cd5e48548842f68b47c538b115a4019d598127f3
Lordimass <lordimass@lordimass.net>
2026-07-14 20:59:14 +0100

    Attempt Netlify Site Fix

--- gaiagauntlet.com/next.config.ts
+++ gaiagauntlet.com/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  distDir: 'out',
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [new URL("https://*.twimg.com/**")]
   }
 };
